fc8d1ea13be8fcf25a6128f672b496a162a4e319
Daniel Viernes <ddvrns@gmail.com>
2017-08-20 20:49:04 +0800

    scratch

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-<?php phpinfo(); ?>
+<?php include 'head.php'; ?>
+<body>
+	<header id="header">
+		<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+
+			<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<a class="navbar-brand" href="#">danielviernes.io</a>
+
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Blog</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Code</a>
+						</li>
+					</ul>
+					<form class="form-inline my-2 my-lg-0">
+						<input class="form-control mr-sm-2" type="text" placeholder="Search">
+						<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+					</form>
+				</div>
+			</nav>
+		</header>
+
+		<?php include 'footer.php'; ?>
+
+
+
